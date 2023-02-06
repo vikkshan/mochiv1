f6daf4599e2d36d816ee319475ca5a7f1d14ede0
Vikshan <103361575+vikkshan@users.noreply.github.com>
2023-02-06 10:35:45 +0000

    added client id to the variables to make the code universal and added human readable explanation

--- src/functions/handelCommands.js
+++ src/functions/handelCommands.js
@@ -1,40 +1,56 @@
+// Importing required modules
 const { REST } = require("@discordjs/rest");
 const { Routes } = require('discord-api-types/v9');
 const fs = require('fs');
 
-const clientId = '1009149789914546287'; 
+// Declaring a constant for the Guild ID
 const guildId = '1015941854124060683'; 
 
+// Exporting a function that receives a "client" object as its argument
 module.exports = (client) => {
-    client.handleCommands = async (commandFolders, path) => {
-        client.commandArray = [];
-        for (folder of commandFolders) {
-            const commandFiles = fs.readdirSync(`${path}/${folder}`).filter(file => file.endsWith('.js'));
-            for (const file of commandFiles) {
-                const command = require(`../commands/${folder}/${file}`);
-                client.commands.set(command.data.name, command);
-                client.commandArray.push(command.data.toJSON());
-            }
-        }
+  // Adding a new property to the client object named "handleCommands"
+  client.handleCommands = async (commandFolders, path) => {
+    // Creating an array property on the client object to store the loaded commands
+    client.commandArray = [];
+    // Loop through each folder in the commandFolders array
+    for (folder of commandFolders) {
+      // Read all the files in the current folder and filter out any that are not JavaScript files
+      const commandFiles = fs.readdirSync(`${path}/${folder}`).filter(file => file.endsWith('.js'));
+      // Loop through each file in the commandFiles array
+      for (const file of commandFiles) {
+        // Require the command file and assign it to the "command" constant
+        const command = require(`../commands/${folder}/${file}`);
+        // Add the command to the "client.commands" Map with its name as the key
+        client.commands.set(command.data.name, command);
+        // Push the command data to the "client.commandArray" array
+        client.commandArray.push(command.data.toJSON());
+      }
+    }
 
-        const rest = new REST({
-            version: '9'
-        }).setToken(process.env.token);
+    // Create a new instance of the REST class and set its token
+    const rest = new REST({
+      version: '9'
+    }).setToken(process.env.token);
 
-        (async () => {
-            try {
-                console.log('Started refreshing application (/) commands.');
+    // Wrap the code in an anonymous asynchronous function
+    (async () => {
+      try {
+        // Log that the refresh process has started
+        console.log('Started refreshing application (/) commands.');
 
-                await rest.put(
-                    Routes.applicationCommands(clientId), {
-                        body: client.commandArray
-                    },
-                );
+        // Make a PUT request to the Discord API to refresh the application commands
+        await rest.put(
+          Routes.applicationCommands(process.env.clientId), {
+            body: client.commandArray
+          },
+        );
 
-                console.log('Successfully reloaded application (/) commands.');
-            } catch (error) {
-                console.error(error);
-            }
-        })();
-    };
+        // Log that the refresh process was successful
+        console.log('Successfully reloaded application (/) commands.');
+      } catch (error) {
+        // Log the error if it occurs
+        console.error(error);
+      }
+    })();
+  };
 };
