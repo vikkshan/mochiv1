4f22ddfa775e776ebfffcf76ac1695a18ff80d81
Muendo Mutu <103361575+vikkshan@users.noreply.github.com>
2023-02-07 10:14:07 +0000

    Added MongoDB connection

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,25 @@
+const mongoose = require('mongoose');
+const mongodbURL = process.env.MONGODBURL;
+
 module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
         console.log('Ready!');
 
+        if (!mongodbURL) return;
+
+        await mongoose.connect(mongodbURL || ``,{
+            keepAlive: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .catch(error => console.error(error));
+        
+        if (mongoose.connect) {
+            console.log('Connected to MongoDB!');
+        }
+        
         async function pickPresence () {
             const option = Math.floor(Math.random() * statusArray.length);
 
